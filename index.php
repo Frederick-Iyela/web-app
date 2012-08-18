ed4acc78c398b6ef97c8013ff1f99d606aafe579
duniyagwan <duniyagwan@gmail.com>
2012-08-17 22:14:48 -0500

    Corrected CSS file path.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Abunai! (Watch Out!)</title>
-		<link href="general.css" rel="stylesheet">
+		<link href="css/general.css" rel="stylesheet">
 	</head>
 	
 	<body>
